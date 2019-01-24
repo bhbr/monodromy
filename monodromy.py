ff1230d0d18d91c30b12a5a603bb7f876bba7893
Ben Hambrecht <benhbr@gmail.com>
2019-01-24 13:03:20 +0100

    single function plot, movable preimages

--- monodromy.py
+++ monodromy.py
@@ -7,10 +7,10 @@ import numpy.polynomial.polynomial as npp
 import numdifftools as nd
 from matplotlib.colors import hsv_to_rgb
 
-dim = 301
-scale = 2
-dim_scaled = scale * dim
-circle_size = 10
+dim = 601
+scale = 1
+dim_scaled = int(scale * dim)
+circle_size = 5
 font_size = 30
 
 values = np.ndarray((dim,dim))
@@ -44,7 +44,7 @@ mouseDown = False
 n = 3
 tau = 2*np.pi
 e = np.cos(tau/n) + 1j * np.sin(tau/n)
-initialRoots = [1+0j, 1.5+0j, (1+0j)*e, (1.5+0j)*e, (1+0j)*e**2, (1.5+0j)*e**2]
+initialRoots = [1j, -1j] # [1+0j, 1.5+0j, (1+0j)*e, (1.5+0j)*e, (1+0j)*e**2, (1.5+0j)*e**2]
 nbRoots = len(initialRoots)
 myRoots = initialRoots
 myRootValues = []
@@ -53,32 +53,37 @@ f_as_poly = npp.polyfromroots(myRoots)
 values = np.ones(np.shape(z),dtype=np.complex128)
 
 sister_points = initialRoots
-
-
+function_drawn = False
 
 def redraw():
-    global values
-    values = np.ones(np.shape(z),dtype=np.complex128)
-    for root in myRoots:
-        zprime = z - root
-        values *= zprime
-    
-    HSV[:,:,0] = np.angle(values)/(2*np.pi) + 0.5
-    r2 = np.real(values)**2 + np.imag(values)**2
-    l = np.log(r2) * 0.5
-    m = l - np.floor(l)
-    smear = 0.02
-    invsmear = 1/smear
-    mm = (m < 1 - smear) * m + (m >= 1 - smear) * (-invsmear * m + invsmear)
-    HSV[:,:,2] = 1 - 0.25 * mm
-    
+    global values, function_drawn
 
-    RGB = np.rot90(np.rint(hsv_to_rgb(HSV) * 255),3)
-    # array is incorrectly oriented for some reason, need to rotate
-    pg.surfarray.blit_array(mySurf,RGB)
+    if not function_drawn:
 
-    mySurf_scaled = pg.transform.scale(mySurf,(dim_scaled,dim_scaled))
-    window.blit(mySurf_scaled,(0,0))
+        # values = np.ones(np.shape(z),dtype=np.complex128)
+        # for root in myRoots:
+        #     zprime = z - root
+        #     values *= zprime
+        values = z + 1.0/z
+        
+        HSV[:,:,0] = np.angle(values)/(2*np.pi) + 0.5
+        r2 = np.real(values)**2 + np.imag(values)**2 + 1e-10
+        l = np.log(r2) * 0.5
+        m = l - np.floor(l)
+        smear = 0.02
+        invsmear = 1/smear
+        mm = (m < 1 - smear) * m + (m >= 1 - smear) * (-invsmear * m + invsmear)
+        HSV[:,:,2] = 1 - 0.25 * mm
+        
+
+        RGB = np.rot90(np.rint(hsv_to_rgb(HSV) * 255),3)
+        # array is incorrectly oriented for some reason, need to rotate
+        pg.surfarray.blit_array(mySurf,RGB)
+
+        mySurf_scaled = pg.transform.scale(mySurf,(dim_scaled,dim_scaled))
+        window.blit(mySurf_scaled,(0,0))
+
+        function_drawn = True
     
     drawPreimages()
 
@@ -102,7 +107,7 @@ def drawPreimages():
         ordered_new_sister_points.append(new_sister_points[i])
     sister_points = ordered_new_sister_points
     for (i,sister) in enumerate(sister_points):
-        pg.draw.circle(window, (0,0,0), complex_to_pixel(sister), circle_size + 2, 0)
+        #pg.draw.circle(window, (0,0,0), complex_to_pixel(sister), circle_size + 2, 0)
         a = 1-i/(nbRoots-1)
         pg.draw.circle(window, (a*255, a*255, a*255), complex_to_pixel(sister), circle_size, 0)
     
@@ -136,7 +141,6 @@ def complex_to_pixel(z0):
 
 
 def findSister(mouseZ):
-    print(sister_points)
     for p in sister_points:
         if (np.real(p)-np.real(mouseZ))**2 + (np.imag(p)-np.imag(mouseZ))**2 <= 0.3**2:
             return p
@@ -159,7 +163,6 @@ while running:
             (mouseX,mouseY) = pg.mouse.get_pos()
             mouseZ = pixel_to_complex(mouseX,mouseY)
             dragging = foundSister(mouseZ)
-            print(dragging)
             
         elif myEvent.type == pg.MOUSEMOTION and dragging:
             # dragging
